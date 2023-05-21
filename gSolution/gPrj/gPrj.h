8f7f509fdeb0779307349ed7c019df58dcf4388c
yeongwoo49 <kristofer00@naver.com>
2023-05-21 10:38:08 +0900

    new dialog

---
+++ gSolution/gPrj/gPrj.h
@@ -0,0 +1,32 @@
+﻿
+// gPrj.h: PROJECT_NAME 애플리케이션에 대한 주 헤더 파일입니다.
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'pch.h'를 포함합니다."
+#endif
+
+#include "resource.h"		// 주 기호입니다.
+
+
+// CgPrjApp:
+// 이 클래스의 구현에 대해서는 gPrj.cpp을(를) 참조하세요.
+//
+
+class CgPrjApp : public CWinApp
+{
+public:
+	CgPrjApp();
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+
+// 구현입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CgPrjApp theApp;
